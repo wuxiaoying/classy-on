c0ec8225128f754d705900148e9204752a1cbbb0
helen <helen@vm-1.helen.koding.kd.io>
2014-09-20 21:12:43 +0000

    setup stuff

---
+++ test/unit/classy.on.spec.coffee
@@ -0,0 +1,42 @@
+﻿# Test Data
+
+app = angular.module 'classyOnTest', [
+    'classy'
+    'classy-on'
+]
+        
+app.classy.controller
+    name: 'OnController'
+   
+    inject: ['$scope']
+   
+    on: 
+        'testEvent': ->
+            @$.eventTriggered = true
+            return
+        
+# Tests
+
+describe 'Classy on (classy-on.coffee)', ->
+    beforeEach module 'classyOnTest'
+    
+    onController = null
+    scope = null
+    rootScope = null
+    
+    beforeEach ->
+        inject ($controller, $rootScope) ->
+            rootScope = $rootScope
+            scope = $rootScope.$new()
+            onController = $controller 'OnController', 
+                $scope: scope
+            return
+        return
+    
+    it 'should register the event', ->
+        expect(scope.eventTriggered).toBeFalsy()
+        rootScope.$broadcast 'testEvent'
+        rootScope.$digest()
+        expect(scope.eventTriggered).toBeTruthy()
+        return
+    return
---
+++ test/unit/classy.on.spec.js
@@ -0,0 +1,39 @@
+﻿(function() {
+  var app;
+
+  app = angular.module('classyOnTest', ['classy', 'classy-on']);
+
+  app.classy.controller({
+    name: 'OnController',
+    inject: ['$scope'],
+    on: {
+      'testEvent': function() {
+        this.$.eventTriggered = true;
+      }
+    }
+  });
+
+  describe('Classy on (classy-on.coffee)', function() {
+    var onController, rootScope, scope;
+    beforeEach(module('classyOnTest'));
+    onController = null;
+    scope = null;
+    rootScope = null;
+    beforeEach(function() {
+      inject(function($controller, $rootScope) {
+        rootScope = $rootScope;
+        scope = $rootScope.$new();
+        onController = $controller('OnController', {
+          $scope: scope
+        });
+      });
+    });
+    it('should register the event', function() {
+      expect(scope.eventTriggered).toBeFalsy();
+      rootScope.$broadcast('testEvent');
+      rootScope.$digest();
+      expect(scope.eventTriggered).toBeTruthy();
+    });
+  });
+
+}).call(this);
