934293eee55d5dff77aa2d80e26498257f8efd28
helenxywu <xiaoyingw12@gmail.com>
2014-09-20 14:45:34 -0700

    zz

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
       'bower_components/angular/angular.js',
       'bower_components/angular-classy/angular-classy.js',
       'classy-on.js',
-      'bower_components/angular-mocks/angular-mocks.js,
+      'bower_components/angular-mocks/angular-mocks.js',
       'test/unit/*.js'
     ],
 
